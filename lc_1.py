bbbb18e43d9fc96229a8899a83727aad1deb7548
Viraj Kamat <virajk>
2026-08-03 09:17:59 -0500

    My LC prep

---
+++ lc_1.py
@@ -0,0 +1,26 @@
+#https://leetcode.com/problems/two-sum/description/
+
+
+def twoSum(arr, target):
+    balances = {}
+    if arr is None or len(arr) == 0:
+        return None
+
+    for i, x in enumerate(arr):
+        bal = target - x
+        if bal in balances:
+            return [balances[bal], i]
+
+        balances[x] = i
+    return None
+
+
+assert twoSum([2, 7, 11, 15], 9) == [0, 1]
+assert twoSum([3, 2, 4], 6) == [1, 2]
+assert twoSum([3, 3], 6) == [0, 1]
+assert twoSum([2, 3, 5, 6], 11) == [2, 3]
+assert twoSum([1, 2, 3], 7) is None
+assert twoSum([], 5) is None
+assert twoSum(None, 5) is None
+
+print("all tests passed")
---
+++ lc_121.py
@@ -0,0 +1,31 @@
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+
+def computeProfit(prices):
+
+    length = len(prices)
+
+    if prices == None or length == 0:
+        return 0
+
+    minVal = prices[0]
+    profit = 0 
+
+    for i in range(1,length):
+        profit = max(profit, prices[i]-minVal)
+        minVal = min(minVal, prices[i])
+
+    
+    return profit
+
+
+assert computeProfit([7, 1, 5, 3, 6, 4]) == 5
+assert computeProfit([7, 6, 4, 3, 1]) == 0
+assert computeProfit([1, 2]) == 1
+assert computeProfit([2, 1]) == 0
+assert computeProfit([2, 4, 1]) == 2
+assert computeProfit([3, 2, 6, 5, 0, 3]) == 4
+assert computeProfit([1]) == 0
+assert computeProfit([]) == 0
+print("all tests passed")
+
